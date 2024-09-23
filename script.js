5df75452946e7214c407e662db71a41598ef3a2d
stronglitz <161094914+stronglitz@users.noreply.github.com>
2024-09-23 19:08:07 -0300

    ok

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     event.preventDefault(); // Previne o envio padrão do formulário
     alert('Mensagem enviada com sucesso! Em breve responderemos.');
 });
+
+// Função para inscrição nos cursos
+function inscrever(curso) {
+    alert(`Inscrição realizada com sucesso no ${curso}! Você agora pode acessar o curso!`);
+}
